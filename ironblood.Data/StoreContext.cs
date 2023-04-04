fca55cf4ecc7d457971f475c2ce5e04f63248899
cfrizzi <frizzi.6@osu.edu>
2023-04-04 17:31:25 -0400

    Create Database Context

--- ironblood.Data/StoreContext.cs
+++ ironblood.Data/StoreContext.cs
@@ -1,5 +1,14 @@
-﻿namespace ironblood.Data;
-public class Class1;
-{
+﻿using ironblood.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
 
+namespace ironblood.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+        : base(options)
+        { }
+
+        public DbSet<Item> Items {get; set;}
+    }
 }
