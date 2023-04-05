5890f6fe778f27d7e823ea4a278faf00a4aa3d48
Daniel Huynh <ddanielhhuynh@gmail.com>
2023-04-05 18:58:55 -0400

    Added HttpsRedirection

--- ironblood.Domain/Orders/Order.cs
+++ ironblood.Domain/Orders/Order.cs
@@ -7,7 +7,7 @@ namespace ironblood.Domain.Orders
     public class Order
     {
         public int Id { get; set; }
-        public List<OrderItem> Items { get; set; }
+        public List<OrderItem>? Items { get; set; }
         public DateTime CreatedDate { get; set; }
         public decimal TotalPrice => Items.Sum(i => i.Price);
     }
