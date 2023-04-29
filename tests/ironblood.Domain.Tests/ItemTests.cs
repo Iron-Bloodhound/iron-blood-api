82df0c59be41877a2439be3438a765d7c9f2dfc8
cfrizzi <frizzi.6@osu.edu>
2023-04-29 12:03:50 -0400

    part4

--- tests/ironblood.Domain.Tests/ItemTests.cs
+++ tests/ironblood.Domain.Tests/ItemTests.cs
@@ -20,4 +20,17 @@ public class ItemTests
         Assert.AreEqual("Brand", item.Brand);
         Assert.AreEqual(10.00m, item.Price);
     }
+    [TestMethod]
+    public void Can_Create_Add_Rating()
+    {
+        //Arrange
+        var item = new Item("Name", "Description", "Brand", 10.00m);
+        var rating = new Rating(5, "Name", "Review");
+
+        //Act
+        item.AddRating(rating);
+
+        //Assert
+        Assert.AreEqual(rating, item.Ratings[0]);
+    }
 }
