4f582d41f3f23f937af343ed8c4d733ab0c82f90
Daniel Huynh <ddanielhhuynh@gmail.com>
2023-04-29 13:15:23 -0400

    Added cors

--- ironblood.Api/Program.cs
+++ ironblood.Api/Program.cs
@@ -12,6 +12,13 @@ builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite("Data Source=../Registrar.sqlite",
     b => b.MigrationsAssembly("ironblood.Api"))
 );
+
+builder.Services.AddCors(options => {
+    options.AddDefaultPolicy(builder => {
+        builder.WithOrigins("http://localhost::3000").AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -28,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
